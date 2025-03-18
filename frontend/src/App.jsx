483f72e3be7a9428c39929057fa36e875c9a1f83
Nikita <147944464+Magnus-Esq@users.noreply.github.com>
2025-03-19 00:18:35 +0300

    цвет отображается на сайте

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,20 @@ import Lighting from './Lighting';
 import Sensors from './Sensors';
 import './style.css';
 
+// Функция для конвертации RGB в HEX
+const rgbToHex = (rgb) => {
+    // Разделяем строку на компоненты R, G, B
+    const [r, g, b] = rgb.split(',').map(Number);
+
+    // Преобразуем каждое значение в HEX и объединяем
+    const toHex = (value) => {
+        const hex = value.toString(16);
+        return hex.length === 1 ? '0' + hex : hex;
+    };
+
+    return `#${toHex(r)}${toHex(g)}${toHex(b)}`.toUpperCase();
+};
+
 const App = () => {
     // Состояния для данных
     const [temperature, setTemperature] = useState(0);
@@ -34,82 +48,85 @@ const App = () => {
 
     // Функция для получения данных с сервера
     const fetchData = () => {
-    fetch('/data') // GET-запрос для получения данных
-        .then(response => response.json())
-        .then(data => {
-            console.log("Результат запроса:", data);
+        fetch('/data') // GET-запрос для получения данных
+            .then(response => response.json())
+            .then(data => {
+                console.log("Результат запроса:", data);
 
-            setTemperature(data.temperature);
-            setHumidity(data.humidity);
-            setBathHumidity(data.Humidity_bathroom);
+                setTemperature(data.temperature);
+                setHumidity(data.humidity);
+                setBathHumidity(data.Humidity_bathroom);
 
-            setMotion(data.motion);
-            setSmoke(data.smoke);
+                setMotion(data.motion);
+                setSmoke(data.smoke);
 
-            setPompaState(data.Pompa);
+                setPompaState(data.Pompa);
 
-            setFanThreshold(data.TemperaturePorog);
-            setBathFanThreshold(data.VlaznostPorog);
-            setAutoMode(data.Rezimi_Klimat_Kontrol);
-            setBathAutoMode(data.ModeVentilation);
+                setFanThreshold(data.TemperaturePorog);
+                setBathFanThreshold(data.VlaznostPorog);
+                setAutoMode(data.Rezimi_Klimat_Kontrol);
+                setBathAutoMode(data.ModeVentilation);
 
-	    setLampColor(data.toggleRGB);
+                // Конвертация RGB в HEX и обновление состояния
+                if (data.toggleRGB) {
+                    const hexColor = rgbToHex(data.toggleRGB);
+                    setLampColor(hexColor);
+                }
 
-            // Обновление состояния устройств с учетом данных с сервера
-            if (data.State_of_Lamp_Bedroom !== led2State) {
-                setLed2State(data.State_of_Lamp_Bedroom === "true"); 
-            }
+                // Обновление состояния устройств с учетом данных с сервера
+                if (data.State_of_Lamp_Bedroom !== led2State) {
+                    setLed2State(data.State_of_Lamp_Bedroom === "true");
+                }
 
-            if (data.State_of_Ventilation !== fanState) {
-                setFanState(data.State_of_Ventilation === "true"); 
-            }
+                if (data.State_of_Ventilation !== fanState) {
+                    setFanState(data.State_of_Ventilation === "true");
+                }
 
-            if (data.State_of_Klimat_Kontrol !== klimatFanState) {
-                setKlimatFanState(data.State_of_Klimat_Kontrol === "true"); 
-            }
+                if (data.State_of_Klimat_Kontrol !== klimatFanState) {
+                    setKlimatFanState(data.State_of_Klimat_Kontrol === "true");
+                }
 
-	    if (data.Pompa !== pompaState) {
-                setPompaState(data.Pompa === "true"); 
-            }
+                if (data.Pompa !== pompaState) {
+                    setPompaState(data.Pompa === "true");
+                }
 
-            if (data.Rezimi_Klimat_Kontrol !== autoMode) {
-                setAutoMode(data.Rezimi_Klimat_Kontrol === "true"); 
-            }
+                if (data.Rezimi_Klimat_Kontrol !== autoMode) {
+                    setAutoMode(data.Rezimi_Klimat_Kontrol === "true");
+                }
 
-	    if (data.ModeVentilation !== bathAutoMode) {
-                setBathAutoMode(data.ModeVentilation === "true"); 
-            }
+                if (data.ModeVentilation !== bathAutoMode) {
+                    setBathAutoMode(data.ModeVentilation === "true");
+                }
 
-            // Добавление уведомлений
-            if (data.motion === "1") {
-                setMotion("Движение обнаружено");
-		addNotification("Движение обнаружено");
-            } else {
-                setMotion("Нет движения");
-	    }
+                // Добавление уведомлений
+                if (data.motion === "1") {
+                    setMotion("Движение обнаружено");
+                    addNotification("Движение обнаружено");
+                } else {
+                    setMotion("Нет движения");
+                }
 
-            if (data.smoke === "1") {
-		setSmoke("Газ обнаружен");
-		addNotification("Газ обнаружен");
-            } else {
-		setSmoke("Не обнаружен");
-	    }
+                if (data.smoke === "1") {
+                    setSmoke("Газ обнаружен");
+                    addNotification("Газ обнаружен");
+                } else {
+                    setSmoke("Не обнаружен");
+                }
 
-	    if (data.Protechka === "1") {
-		setProtechka("Протечка обнаружена");
-                addNotification("Протечка обнаружена");
-            } else {
-	        setProtechka("Не обнаружена");
-	    }
-        })
-        .catch(err => console.error("Ошибка получения данных:", err));
-};
-
+                if (data.Protechka === "1") {
+                    setProtechka("Протечка обнаружена");
+                    addNotification("Протечка обнаружена");
+                } else {
+                    setProtechka("Не обнаружена");
+                }
+            })
+            .catch(err => console.error("Ошибка получения данных:", err));
+    };
 
     // Запуск периодического обновления данных
     useEffect(() => {
         fetchData(); // Первоначальный запрос данных
-        const interval = setInterval(fetchData, 1000); // Обновление каждые 0.5 секунды
+        const interval = setInterval(fetchData, 1000); // Обновление каждую секунду
         return () => clearInterval(interval); // Очистка интервала при размонтировании
     }, []);
 
@@ -122,104 +139,103 @@ const App = () => {
     };
 
     const toggleLED2 = () => {
-    const newState = !led2State; // Новое состояние переключателя
-    fetch('/api/toggle-module/RGBLenta_Bedroom', { method: 'POST' })
-        .then(response => response.json())
-        .then(data => {
-            // Проверяем, что пришел ответ с актуальным состоянием устройства
-            if (data.State_of_Lamp_Bedroom !== undefined) {
-                const actualState = data.State_of_Lamp_Bedroom === "true"; // Преобразуем строку в булево значение
-                setLed2State(actualState); // Обновляем состояние на фронте
-            } else {
-                console.error("Некорректный ответ от сервера:", data);
+        const newState = !led2State; // Новое состояние переключателя
+        fetch('/api/toggle-module/RGBLenta_Bedroom', { method: 'POST' })
+            .then(response => response.json())
+            .then(data => {
+                // Проверяем, что пришел ответ с актуальным состоянием устройства
+                if (data.State_of_Lamp_Bedroom !== undefined) {
+                    const actualState = data.State_of_Lamp_Bedroom === "true"; // Преобразуем строку в булево значение
+                    setLed2State(actualState); // Обновляем состояние на фронте
+                } else {
+                    console.error("Некорректный ответ от сервера:", data);
+                    setLed2State(newState); // Откат состояния в случае ошибки
+                }
+            })
+            .catch(err => {
+                console.error("Ошибка переключения лампы:", err);
                 setLed2State(newState); // Откат состояния в случае ошибки
-            }
-        })
-        .catch(err => {
-            console.error("Ошибка переключения лампы:", err);
-            setLed2State(newState); // Откат состояния в случае ошибки
-        });
+            });
     };
 
-
     const toggleFan = () => {
-    const newState = !fanState; // Новое состояние переключателя (переключаемся на противоположное)
-    fetch('/api/toggle-module/powerVentilation', { method: 'POST' })
-        .then(response => response.json())
-        .then(data => {
-            // Проверяем, что пришел ответ с актуальным состоянием устройства
-            if (data.State_of_Ventilation !== undefined) {
-                const actualState = data.State_of_Ventilation === "true"; // Преобразуем строку в булево значение
-                setFanState(actualState); // Обновляем состояние фронта
-            } else {
-                console.error("Некорректный ответ от сервера:", data);
+        const newState = !fanState; // Новое состояние переключателя (переключаемся на противоположное)
+        fetch('/api/toggle-module/powerVentilation', { method: 'POST' })
+            .then(response => response.json())
+            .then(data => {
+                // Проверяем, что пришел ответ с актуальным состоянием устройства
+                if (data.State_of_Ventilation !== undefined) {
+                    const actualState = data.State_of_Ventilation === "true"; // Преобразуем строку в булево значение
+                    setFanState(actualState); // Обновляем состояние фронта
+                } else {
+                    console.error("Некорректный ответ от сервера:", data);
+                    setFanState(newState); // Откат состояния в случае ошибки
+                }
+            })
+            .catch(err => {
+                console.error("Ошибка переключения вентилятора:", err);
                 setFanState(newState); // Откат состояния в случае ошибки
-            }
-        })
-        .catch(err => {
-            console.error("Ошибка переключения вентилятора:", err);
-            setFanState(newState); // Откат состояния в случае ошибки
-        });
+            });
     };
 
     const toggleKlimatFan = () => {
-    const newState = !klimatFanState; // Новое состояние переключателя (переключаемся на противоположное)
-    fetch('/api/toggle-module/Klimat_Kontrol', { method: 'POST' })
-        .then(response => response.json())
-        .then(data => {
-            // Проверяем, что пришел ответ с актуальным состоянием устройства
-            if (data.State_of_Klimat_Kontrol !== undefined) {
-                const actualState = data.State_of_Klimat_Kontrol === "true"; // Преобразуем строку в булево значение
-                setKlimatFanState(actualState); // Обновляем состояние фронта
-            } else {
-                console.error("Некорректный ответ от сервера:", data);
+        const newState = !klimatFanState; // Новое состояние переключателя (переключаемся на противоположное)
+        fetch('/api/toggle-module/Klimat_Kontrol', { method: 'POST' })
+            .then(response => response.json())
+            .then(data => {
+                // Проверяем, что пришел ответ с актуальным состоянием устройства
+                if (data.State_of_Klimat_Kontrol !== undefined) {
+                    const actualState = data.State_of_Klimat_Kontrol === "true"; // Преобразуем строку в булево значение
+                    setKlimatFanState(actualState); // Обновляем состояние фронта
+                } else {
+                    console.error("Некорректный ответ от сервера:", data);
+                    setKlimatFanState(newState); // Откат состояния в случае ошибки
+                }
+            })
+            .catch(err => {
+                console.error("Ошибка переключения вентилятора:", err);
                 setKlimatFanState(newState); // Откат состояния в случае ошибки
-            }
-        })
-        .catch(err => {
-            console.error("Ошибка переключения вентилятора:", err);
-            setKlimatFanState(newState); // Откат состояния в случае ошибки
-        });
+            });
     };
 
     const togglePompa = () => {
-    const newState = !pompaState; // Новое состояние переключателя (переключаемся на противоположное)
-    fetch('/api/toggle-module/Pompa', { method: 'POST' })
-        .then(response => response.json())
-        .then(data => {
-            // Проверяем, что пришел ответ с актуальным состоянием устройства
-            if (data.Pompa !== undefined) {
-                const actualState = data.Pompa === "true"; // Преобразуем строку в булево значение
-                setPompaState(actualState); // Обновляем состояние фронта
-            } else {
-                console.error("Некорректный ответ от сервера:", data);
+        const newState = !pompaState; // Новое состояние переключателя (переключаемся на противоположное)
+        fetch('/api/toggle-module/Pompa', { method: 'POST' })
+            .then(response => response.json())
+            .then(data => {
+                // Проверяем, что пришел ответ с актуальным состоянием устройства
+                if (data.Pompa !== undefined) {
+                    const actualState = data.Pompa === "true"; // Преобразуем строку в булево значение
+                    setPompaState(actualState); // Обновляем состояние фронта
+                } else {
+                    console.error("Некорректный ответ от сервера:", data);
+                    setPompaState(newState); // Откат состояния в случае ошибки
+                }
+            })
+            .catch(err => {
+                console.error("Ошибка переключения вентилятора:", err);
                 setPompaState(newState); // Откат состояния в случае ошибки
-            }
-        })
-        .catch(err => {
-            console.error("Ошибка переключения вентилятора:", err);
-            setPompaState(newState); // Откат состояния в случае ошибки
-        });
+            });
     };
 
     const setFanThresholdValue = () => {
-    const threshold = document.getElementById('fanThresholdInput').value;
-    fetch('/api/setFanThreshold/TemperaturePorog', { method: 'POST', headers: { 'Content-Type': 'application/json'}, body: JSON.stringify({ threshold })})
-	.then(response => response.text())
-	.then(text => {
-	    setFanThreshold(threshold);
-	})
-	.catch(err => console.error("Ошибка установки порога вентилятора:", err));
+        const threshold = document.getElementById('fanThresholdInput').value;
+        fetch('/api/setFanThreshold/TemperaturePorog', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ threshold }) })
+            .then(response => response.text())
+            .then(text => {
+                setFanThreshold(threshold);
+            })
+            .catch(err => console.error("Ошибка установки порога вентилятора:", err));
     };
 
     const setBathFanThresholdValue = () => {
-    const threshold = document.getElementById('bathFanThresholdInput').value;
-    fetch('/api/setFanThreshold/VlaznostPorog', { method: 'POST', headers: { 'Content-Type': 'application/json'}, body: JSON.stringify({ threshold })})
-	.then(response => response.text())
-	.then(text => {
-	    setBathFanThreshold(threshold);
-	})
-	.catch(err => console.error("Ошибка установки порога вентилятора:", err));
+        const threshold = document.getElementById('bathFanThresholdInput').value;
+        fetch('/api/setFanThreshold/VlaznostPorog', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ threshold }) })
+            .then(response => response.text())
+            .then(text => {
+                setBathFanThreshold(threshold);
+            })
+            .catch(err => console.error("Ошибка установки порога вентилятора:", err));
     };
 
     const toggleAutoMode = () => {
@@ -241,16 +257,16 @@ const App = () => {
     };
 
     const toggleLampColor = (color) => {
-    fetch('/api/set-light-color', { method: 'POST', headers: { 'Content-Type': 'application/json'}, body: JSON.stringify({ color })})
-    	.then(response => response.json())
-    	.then(data => {
-	    if (data.status === "success") {
-            	setLampColor(color); // Обновляем состояние цвета на фронте
-            } else {
-            console.error("Ошибка установки цвета:", data.message);
-            }
-        })
-        .catch(err => console.error("Ошибка сети:", err));
+        fetch('/api/set-light-color', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ color }) })
+            .then(response => response.json())
+            .then(data => {
+                if (data.status === "success") {
+                    setLampColor(color); // Обновляем состояние цвета на фронте
+                } else {
+                    console.error("Ошибка установки цвета:", data.message);
+                }
+            })
+            .catch(err => console.error("Ошибка сети:", err));
     };
 
     return (
@@ -280,17 +296,17 @@ const App = () => {
                 <Lighting
                     led2State={led2State}
                     toggleLED2={toggleLED2}
-    		    lampColor={lampColor}
-    		    toggleLampColor={toggleLampColor}
+                    lampColor={lampColor}
+                    toggleLampColor={toggleLampColor}
                 />
             )}
             {activeTab === 'Sensors' && (
                 <Sensors
                     motion={motion}
                     smoke={smoke}
-		    protechka={protechka}
-		    pompaState={pompaState}
-		    togglePompa={togglePompa}
+                    protechka={protechka}
+                    pompaState={pompaState}
+                    togglePompa={togglePompa}
                     notifications={notifications}
                 />
             )}
